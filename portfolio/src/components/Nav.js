612f93b50355298b9ce0e65e5853ac86080c9045
jacknel <mjacknelson19@gmail.com>
2023-01-23 06:22:35 -0600

    added components and got nav working

---
+++ portfolio/src/components/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+function Nav() {
+    return (
+        <nav className="nav">
+            <h1>portCOOL</h1>
+            <a href='#'>about me</a>
+            <a href='#'>portfolio</a>
+            <a href='#'>contact</a>
+            <a href='#'>resume</a>
+        </nav>
+    );
+}
+
+export default Nav;
